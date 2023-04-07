1c30a3862b07b47222c1f8a4fd32e26797273448
ChrisRDavila <crossdavila@gmail.com>
2023-04-07 11:28:14 -0700

    Test 3 passes, will use array to designate property for pizza style

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 //Business Logic for Pizza-----------
 
 const sizes = ["small", "medium", "large"];
+const styles = ["regular", "deep-dish", "thin-crust"]
 
-function Pizza(size, style, sauce, meatTop, vegTop) {
+function Pizza(size, style, sauce, meatTop, vegTop, crust) {
 this.size = this.chooseSize(size);
-this.style = style;
+this.style = this.chooseStyle(style);
 this.sauce = sauce;
 this.meatTop = meatTop;
 this.vegTop = vegTop;
+this.crust = crust;
 }
-
-
-
 
 Pizza.prototype.chooseSize = function(size) {
   return sizes[size]; 
 }
 
-let testPizza = new Pizza(0, "detroit", "red", "pepperoini", "olive");
+Pizza.prototype.chooseStyle = function(style) {
+  return styles[style];
+}
+
+let testPizza = new Pizza(0, 0, "red", "pepperoini", "olive");
