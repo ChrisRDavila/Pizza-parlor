3c1b10819cd335a5e9fd47b08ec96ad2cb419bad
ChrisRDavila <crossdavila@gmail.com>
2023-04-07 14:46:24 -0700

    test 6 now test for if Pizza() property can pull from veggie array for veggie choice, test passed, function built

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,8 @@
 const sizes = ["small", "medium", "large"];
 const styles = ["regular", "deep-dish", "thin-crust"]
 const sauces = ["red", "white", "BBQ"];
-let meats = [
-  {id: 0, value: "none"},
-  {id: 1, value: "pepperoni"},
-  {id: 2, value: "sausage"}
-]
-  let ids = meats.map((item)=>item.id);
-  let values = meats.map((item)=>item.value); 
+const meats = ["none", "pepperoni", "sausage"];
+const veggies = ["none", "olive", "bell-pepper"];
 
 
 
@@ -17,9 +12,8 @@ function Pizza(size, style, sauce, meatTop, vegTop, crust) {
 this.size = this.chooseSize(size);
 this.style = this.chooseStyle(style);
 this.sauce = this.chooseSauce(sauce);
-this.meatTop = this.chooseMeat[meatTop];
-this.vegTop = vegTop;
-this.crust = crust;
+this.meatTop = this.chooseMeat(meatTop);
+this.vegTop = this.chooseVeg(vegTop);
 }
 
 Pizza.prototype.chooseSize = function(size) {
@@ -35,5 +29,8 @@ Pizza.prototype.chooseSauce = function(sauce) {
 Pizza.prototype.chooseMeat = function(meatTop) {
   return meats[meatTop];
 }
+Pizza.prototype.chooseVeg = function(vegTop) {
+  return veggies[vegTop];
+}
  
-let testPizza = new Pizza(0, 0, 0, 0,"olives");
+let testPizza = new Pizza(0, 0, 0, 0, 0);
