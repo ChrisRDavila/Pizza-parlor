d6312136013b83f11b5a06e48182e0fc95a16d1b
ChrisRDavila <crossdavila@gmail.com>
2023-04-07 11:41:40 -0700

    Test 4 for Pizza() passed, function will now use array to choose sauce type for new Pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@
 
 const sizes = ["small", "medium", "large"];
 const styles = ["regular", "deep-dish", "thin-crust"]
+const sauces = ["red", "white", "BBQ"];
 
 function Pizza(size, style, sauce, meatTop, vegTop, crust) {
 this.size = this.chooseSize(size);
 this.style = this.chooseStyle(style);
-this.sauce = sauce;
+this.sauce = this.chooseSauce(sauce);
 this.meatTop = meatTop;
 this.vegTop = vegTop;
 this.crust = crust;
@@ -19,5 +20,8 @@ Pizza.prototype.chooseSize = function(size) {
 Pizza.prototype.chooseStyle = function(style) {
   return styles[style];
 }
+Pizza.prototype.chooseSauce = function(sauce) {
+  return sauces[sauce];
+}
 
-let testPizza = new Pizza(0, 0, "red", "pepperoini", "olive");
+let testPizza = new Pizza(0, 0, 0, "pepperoini", "olive");
