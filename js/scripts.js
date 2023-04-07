c3c2721ea6c4631267bb1177d90a6155d2d57dff
ChrisRDavila <crossdavila@gmail.com>
2023-04-07 15:59:44 -0700

    not sure constructor map will declutter my code as much as id hoped, going to set up tests for value and id assignment to each property now

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 //Business Logic for Pizza-----------
 
-//const sizes = [
-//  {id: 1, name: "small", value: 10}, 
-//  {id: 2, name: "medium", value: 13},
-//  {id: 3, name: "large", value: 16}
-//];
-//let ids = sizes.map( (item) => item.id);
-//let names = sizes.map((item) => item.name);
-//let values = sizes.map((item) => item.value);
+const sizes = [
+  {id: 1, name: "small", value: 10}, 
+  {id: 2, name: "medium", value: 13},
+  {id: 3, name: "large", value: 16}
+];
+let ids = sizes.map( (item) => item.id);
+let names = sizes.map((item) => item.name);
+let values = sizes.map((item) => item.value);
 
-const styles = ["regular", "deep-dish", "thin-crust"]
+const styles = 
+["regular", "deep-dish", "thin-crust"]
 const sauces = ["red", "white", "BBQ"];
 const meats = ["none", "pepperoni", "sausage"];
 const veggies = ["none", "olive", "bell-pepper"];
@@ -50,19 +51,18 @@ let testPizza = new Pizza(0, 0, 0, [1, 2], 0);
 
 //Business Logic for Map properties
 
-function PropertyMap = (id, name, value) {
-  this.id = id;
-  this.name = name;
-  this.value = value;
-}
-
-const SmallSizeMap = new Map([
-  [id, 0],
-  ["name", "small"],
-  [value, 10]
-])
-const MediumSizeMap =  new Map([
-  [id, 1],
-  ["name", "small"],
-
-])
+//function PropertyMap(id, name, value) {
+//  this.id = id;
+//  this.name = name;
+//  this.value = value;
+//} 
+//const SmallSizeMap = new PropertyMap([
+//  [id, 0],
+//  ["name", "small"],
+//  [value, 10]
+//])
+//const MediumSizeMap =  new Map([
+//  [id, 1],
+//  ["name", "small"],
+//  [value, 13]
+//])
