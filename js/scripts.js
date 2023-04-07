475d6917b6a3c260be9015a515806c9d17cb16ab
ChrisRDavila <crossdavila@gmail.com>
2023-04-07 09:38:57 -0700

    first test passes, designed first function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //Business Logic for Pizza-----------
-//function Pizza(size, style, sauce, meatTop,vegTop) {
-//this.size = size;
-//this.style = style;
-//this.sauce = sauce;
-//this.meatTop = meatTop;
-//this.vegTop = vegTop;
+function Pizza(size, style, sauce, meatTop,vegTop) {
+this.size = size;
+this.style = style;
+this.sauce = sauce;
+this.meatTop = meatTop;
+this.vegTop = vegTop;
 }
 
 
