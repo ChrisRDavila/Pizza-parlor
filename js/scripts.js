fa6c931d6a20889f5b63195363b690495188f2da
ChrisRDavila <crossdavila@gmail.com>
2023-04-09 21:59:16 -0700

    bug fixed

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,10 @@ this.toppings = toppings;
 this.sides = sides;
 this.delivery = delivery
 this.price = this.getCost();
+delivery = Boolean
 }
-// Pizza.prototype.sizePrice = function() {
-//   if (this.size === "small"){
-//     this.price = 10;
-//   }
-//   else if (this.size === "medium") {
-//     this.price = 13;
-//   }
-//   else {
-//     this.price = 16;
-//   }
-//   return this.price
-// }
-// Pizza.prototype.toppingsPrice = function() {
-//    return this.price += (this.toppings.lenght * 2);
-// }
-// Pizza.prototype.sidesPrice = function() {
-//   return this.price += (this.sides.length);
-// }
-// Pizza.prototype.deliveryPrice = function() {
-//   if (this.delivery === true) {
-//     return this.price
-//   }
-// }
 Pizza.prototype.getCost = function() {
   let cost = 10;
-  this.delivery = [Boolean];
   if (this.size === "medium") {
     cost += 3;
   } else if (this.size === "large") {
@@ -63,16 +40,16 @@ function handleFormSubmission(event) {
   const toppingOptions = document.querySelectorAll(".top-class:checked");
   const selectedToppings =[];
   for (i = 0; i < toppingOptions.length; i ++) {
-      selectedToppings.push((toppingOptions.value));
+      selectedToppings.push((toppingOptions[i].value));
     }
   const sideOptions = document.querySelectorAll(".side-class:checked");
   const selectedSides = [];
   for (i = 0; i < sideOptions.length; i ++) {
-      selectedSides.push(sideOptions.value);
+      selectedSides.push(sideOptions[i].value);
     }  
-  const inputtedDelivery = document.querySelector("#yes-delivery").value;
-  let pizza = new Pizza(inputtedSize, inputtedStyle, inputtedSauce, toppingOptions, sideOptions, inputtedDelivery);
-  if (inputtedDelivery === true) {
+  const inputtedDelivery = document.querySelector("#yes-delivery:checked").value;
+  let pizza = new Pizza(inputtedSize, inputtedStyle, inputtedSauce, selectedToppings, selectedSides, inputtedDelivery);
+  if (inputtedDelivery.value === true) {
     document.getElementById("delivery-input").style.visibility = "visible";;
   }
   displayNewOrder(pizza);   
